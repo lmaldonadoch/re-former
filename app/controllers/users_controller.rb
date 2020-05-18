82d6c3662d62c8a735f4fa6bd865225ba8f57eed
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-18 11:30:36 -0500

    Fixed an issue with form preventing us to create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
 	@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  @user.username = params[:username]
   
-	if @user.save
+  p params
+  p @user
+  if @user.save
 	  redirect_to new_user_path
 	else
 	  render :new
