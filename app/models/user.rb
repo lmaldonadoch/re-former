38badf96fbe09983757828f867ede8c72c7b01f1
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-18 15:31:22 -0500

    Fixed flash messages and added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, length: { minimum: 5 }, presence: true
   validates :email, presence: true, format: { with: EMAIL_FORMAT }
-  validates :password, presence: true
+  validates :password, presence: true, format: {with: /\A(?=.*[A-Z])/x}
 end
