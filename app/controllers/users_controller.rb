4234b9112ca8d4e0264f6ebbd4fa73d9581f0426
shubham14p3 <shubham14p3@gmail.com>
2020-05-19 01:49:49 +0530

    Added Flash messaged in Update, New modules

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,19 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    @user.email ||= 'example@example.com'
+	@user.email ||= 'example@example.com'
+	@user.username ||= 'Example-Username'
   end
 
   def create
 	  # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     @user = User.new(user_params)
-    if @user.save
+	if @user.save
+	  flash.notice = "User '#{@user.username}' Created"
       redirect_to new_user_path
-    else
+	else
+	  flash.notice= "User '#{@user.username}' not created"
       render :new
     end
   end
@@ -23,10 +26,12 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     
-    if @user.update(user_params)
+	if @user.update(user_params)
+	 flash.notice= "User '#{@user.username}' Updated"
      redirect_to edit_user_path
 
-    else
+	else
+	  flash.notice= "User '#{@user.username}' Error"
       render :edit
     end
   end
